0f97fcafb3b822d164451d040c32722c9baa2c22
Alin Babu <alinbabu2010@gmail.com>
2023-04-24 16:03:28 +0530

    Add model class for tasks

---
+++ lib/models/tasks.dart
@@ -0,0 +1,43 @@
+import 'package:equatable/equatable.dart';
+
+class Task extends Equatable {
+  final String title;
+  bool? isDone;
+  bool? isDeleted;
+
+  Task({required this.title, this.isDone, this.isDeleted}) {
+    isDone ??= false;
+    isDeleted ??= false;
+  }
+
+  Task copyWith({
+    String? title,
+    bool? isDone,
+    bool? isDeleted,
+  }) {
+    return Task(
+      title: title ?? this.title,
+      isDone: isDone ?? this.isDone,
+      isDeleted: isDeleted ?? this.isDeleted,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'isDone': isDone,
+      'isDeleted': isDeleted,
+    };
+  }
+
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      title: map['title'] as String,
+      isDone: map['isDone'] as bool,
+      isDeleted: map['isDeleted'] as bool,
+    );
+  }
+
+  @override
+  List<Object?> get props => [title, isDone, isDeleted];
+}
