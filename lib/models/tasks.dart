dcfae0469b44bdaaf52aa54b58e10132cb4b4a1d
Alin Babu <alinbabu2010@gmail.com>
2023-04-25 12:57:13 +0530

    Add id for task model

--- lib/models/tasks.dart
+++ lib/models/tasks.dart
@@ -2,21 +2,24 @@ import 'package:equatable/equatable.dart';
 
 class Task extends Equatable {
   final String title;
+  final String id;
   bool? isDone;
   bool? isDeleted;
 
-  Task({required this.title, this.isDone, this.isDeleted}) {
+  Task({required this.title, required this.id, this.isDone, this.isDeleted}) {
     isDone ??= false;
     isDeleted ??= false;
   }
 
   Task copyWith({
     String? title,
+    String? id,
     bool? isDone,
     bool? isDeleted,
   }) {
     return Task(
       title: title ?? this.title,
+      id: id ?? this.id,
       isDone: isDone ?? this.isDone,
       isDeleted: isDeleted ?? this.isDeleted,
     );
@@ -25,6 +28,7 @@ class Task extends Equatable {
   Map<String, dynamic> toMap() {
     return {
       'title': title,
+      'id': id,
       'isDone': isDone,
       'isDeleted': isDeleted,
     };
@@ -33,11 +37,12 @@ class Task extends Equatable {
   factory Task.fromMap(Map<String, dynamic> map) {
     return Task(
       title: map['title'] as String,
+      id: map['id'] as String,
       isDone: map['isDone'] as bool,
       isDeleted: map['isDeleted'] as bool,
     );
   }
 
   @override
-  List<Object?> get props => [title, isDone, isDeleted];
+  List<Object?> get props => [title, id, isDone, isDeleted];
 }
