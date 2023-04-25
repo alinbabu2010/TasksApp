3548cc751f0f445d117c47588eab2551e88c26c7
Alin Babu <alinbabu2010@gmail.com>
2023-04-25 18:40:52 +0530

    Add removed tasks list in bloc and implement copy function for TaskState

--- lib/blocs/tasks/tasks_state.dart
+++ lib/blocs/tasks/tasks_state.dart
@@ -2,15 +2,20 @@ part of 'tasks_bloc.dart';
 
 class TasksState extends Equatable {
   final List<Task> allTasks;
+  final List<Task> removedTasks;
 
-  const TasksState({this.allTasks = const <Task>[]});
+  const TasksState({
+    this.allTasks = const <Task>[],
+    this.removedTasks = const <Task>[],
+  });
 
   @override
-  List<Object?> get props => [allTasks];
+  List<Object?> get props => [allTasks, removedTasks];
 
   Map<String, dynamic> toMap() {
     return <String, dynamic>{
       'allTasks': allTasks.map((x) => x.toMap()).toList(),
+      'removedTasks': removedTasks.map((x) => x.toMap()).toList(),
     };
   }
 
@@ -21,6 +26,21 @@ class TasksState extends Equatable {
           (x) => Task.fromMap(x),
         ),
       ),
+      removedTasks: List<Task>.from(
+        map['removedTasks'].map<Task>(
+          (x) => Task.fromMap(x),
+        ),
+      ),
+    );
+  }
+
+  TasksState copyWith({
+    List<Task>? allTasks,
+    List<Task>? removedTasks,
+  }) {
+    return TasksState(
+      allTasks: allTasks ?? this.allTasks,
+      removedTasks: removedTasks ?? this.removedTasks,
     );
   }
 }
