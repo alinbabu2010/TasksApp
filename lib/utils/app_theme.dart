b76c217bd0742b8f437d95a689bf2107e22858b0
Alin Babu <alinbabu2010@gmail.com>
2023-04-26 14:50:31 +0530

    Add bottom navigation theme in app_theme.dart

--- lib/utils/app_theme.dart
+++ lib/utils/app_theme.dart
@@ -13,6 +13,11 @@ class AppThemes {
           secondary: Colors.deepOrangeAccent.shade100,
           onSecondary: Colors.black87),
       dividerColor: Colors.grey.shade300,
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
+        backgroundColor: Colors.deepOrangeAccent.shade100,
+        selectedItemColor: Colors.red.shade900,
+        unselectedItemColor: Colors.black87,
+      ),
     ),
     AppTheme.lightTheme: ThemeData(
       colorScheme: const ColorScheme.light(
@@ -23,6 +28,11 @@ class AppThemes {
           secondary: Colors.deepOrangeAccent,
           onSecondary: Colors.white),
       dividerColor: const Color(0xff757575),
+      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+        backgroundColor: Colors.deepOrangeAccent,
+        selectedItemColor: Colors.yellowAccent,
+        unselectedItemColor: Colors.white,
+      ),
     ),
   };
 }
