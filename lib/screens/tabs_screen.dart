9a31800d4be6c9322316c25151d17237f0272c78
Alin Babu <alinbabu2010@gmail.com>
2023-04-26 16:29:26 +0530

    Add tab pages list map and selected page display in tabs screen
    Add variable to manage current index.
    Refactor screen to stateful widget.
    Add check to show FAB on pending task list screen.

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -5,10 +5,23 @@ import 'package:tasks_app/screens/add_tasks_screen.dart';
 import 'package:tasks_app/screens/task_drawer.dart';
 import 'package:tasks_app/screens/tasks_screen.dart';
 
-class TabsScreen extends StatelessWidget {
+class TabsScreen extends StatefulWidget {
   const TabsScreen({Key? key}) : super(key: key);
 
   static const routeName = "tabs_screen";
+
+  @override
+  State<TabsScreen> createState() => _TabsScreenState();
+}
+
+class _TabsScreenState extends State<TabsScreen> {
+  final List<Map<String, dynamic>> pageListMap = [
+    {"page": const TasksScreen(), "title": "Pending tasks"},
+    {"page": const TasksScreen(isCompleted: true), "title": "Completed tasks"},
+    {"page": const TasksScreen(isFavorite: true), "title": "Favorite tasks"},
+  ];
+
+  var selectedPage = 0;
 
   void _addTasks(BuildContext context) {
     showModalBottomSheet(
@@ -27,9 +40,9 @@ class TabsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Tabs Screen"),
+        title: Text(pageListMap[selectedPage]["title"]),
         actions: [
-          if (Platform.isIOS)
+          if (Platform.isIOS && selectedPage == 0)
             IconButton(
               onPressed: () => _addTasks(context),
               icon: const Icon(Icons.add),
@@ -37,17 +50,21 @@ class TabsScreen extends StatelessWidget {
         ],
       ),
       drawer: const TaskDrawer(),
-      floatingActionButton: Platform.isAndroid
+      floatingActionButton: (Platform.isAndroid && selectedPage == 0)
           ? FloatingActionButton(
               onPressed: () => _addTasks(context),
               tooltip: 'Add Task',
               child: const Icon(Icons.add),
             )
           : null,
-      body: const TasksScreen(),
+      body: pageListMap[selectedPage]["page"],
       bottomNavigationBar: BottomNavigationBar(
-        currentIndex: 0,
-        onTap: (index) {},
+        currentIndex: selectedPage,
+        onTap: (index) {
+          setState(() {
+            selectedPage = index;
+          });
+        },
         items: const [
           BottomNavigationBarItem(
             icon: Icon(Icons.list),
