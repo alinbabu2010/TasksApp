9cb0c1c16258a45be7fce388bcd84dcd6d051cc5
Alin Babu <alinbabu2010@gmail.com>
2023-04-26 12:37:13 +0530

    Add multi bloc provider and theme mode setup for app based on theme bloc
    Also add navigation bar color

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:tasks_app/utils/app_route.dart';
+import 'package:tasks_app/utils/app_theme.dart';
 
 import 'blocs/bloc_exports.dart';
 import 'screens/tasks_screen.dart';
@@ -23,19 +24,33 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
-      statusBarColor: Colors.transparent,
-    ));
-    return BlocProvider(
-      create: (context) => TasksBloc(),
-      child: MaterialApp(
-        title: 'Flutter Tasks App',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        debugShowCheckedModeBanner: false,
-        home: const TasksScreen(),
-        onGenerateRoute: appRouter.onGenerateRoute,
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+        statusBarColor: Colors.transparent,
+        systemNavigationBarColor: Colors.deepOrange.shade200));
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => TasksBloc()),
+        BlocProvider(create: (context) => ThemeBloc()),
+      ],
+      child: BlocBuilder<ThemeBloc, ThemeState>(
+        builder: (context, state) {
+          /*final theme =
+              state.isDarkTheme ? AppTheme.darkTheme : AppTheme.lightTheme;*/
+          final themeMode = state.isSystemTheme
+              ? ThemeMode.system
+              : state.isDarkTheme
+                  ? ThemeMode.dark
+                  : ThemeMode.light;
+          return MaterialApp(
+            title: 'Flutter Tasks App',
+            theme: AppThemes.appThemeData[AppTheme.lightTheme],
+            debugShowCheckedModeBanner: false,
+            darkTheme: AppThemes.appThemeData[AppTheme.darkTheme],
+            home: const TasksScreen(),
+            themeMode: themeMode,
+            onGenerateRoute: appRouter.onGenerateRoute,
+          );
+        },
       ),
     );
   }
