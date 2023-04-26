c2e1af4ef61d1df5dfa02fcc4cae5a93b477d597
Alin Babu <alinbabu2010@gmail.com>
2023-04-26 16:33:14 +0530

    Refactor tasks screen to show tasks list based on specific task type
    Cut scaffold and FAB from screen

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,71 +1,54 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
-import 'package:tasks_app/screens/task_drawer.dart';
 
 import '../blocs/bloc_exports.dart';
 import '../widgets/tasks_list.dart';
-import 'add_tasks_screen.dart';
 
-class TasksScreen extends StatefulWidget {
-  const TasksScreen({Key? key}) : super(key: key);
+class TasksScreen extends StatelessWidget {
+  final bool? isCompleted;
+  final bool? isFavorite;
+
+  const TasksScreen({
+    Key? key,
+    this.isCompleted,
+    this.isFavorite,
+  }) : super(key: key);
 
   static const routeName = "tasks_screen";
-
-  @override
-  State<TasksScreen> createState() => _TasksScreenState();
-}
-
-class _TasksScreenState extends State<TasksScreen> {
-  void _addTasks(BuildContext context) {
-    showModalBottomSheet(
-      context: context,
-      builder: (context) => SingleChildScrollView(
-        child: Container(
-          padding:
-              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
-          child: const AddTasksScreen(),
-        ),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<TasksBloc, TasksState>(
       builder: (context, state) {
-        return Scaffold(
-          appBar: AppBar(
-            title: const Text('Tasks App'),
-            actions: [
-              if (Platform.isIOS)
-                IconButton(
-                  onPressed: () => _addTasks(context),
-                  icon: const Icon(Icons.add),
-                )
-            ],
-          ),
-          body: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Center(
-                child: Chip(
-                  label: Text(
-                    '${state.allTasks.length} Tasks',
+        final pendingTaskLength = state.pendingTasks.length;
+        final completedTaskLength = state.completedTasks.length;
+        final favoriteTaskLength = state.favoriteTasks.length;
+
+        var tasks = isCompleted == true
+            ? state.completedTasks
+            : isFavorite == true
+                ? state.favoriteTasks
+                : state.pendingTasks;
+
+        return Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Center(
+              child: Chip(
+                label: AnimatedSize(
+                  duration: const Duration(milliseconds: 150),
+                  curve: Curves.fastOutSlowIn,
+                  child: Text(
+                    isCompleted == true
+                        ? "$completedTaskLength Tasks"
+                        : isFavorite == true
+                            ? "$favoriteTaskLength Tasks"
+                            : "$pendingTaskLength Pending | $completedTaskLength Completed",
                   ),
                 ),
               ),
-              TasksList(taskList: state.allTasks)
-            ],
-          ),
-          drawer: const TaskDrawer(),
-          floatingActionButton: Platform.isAndroid
-              ? FloatingActionButton(
-                  onPressed: () => _addTasks(context),
-                  tooltip: 'Add Task',
-                  child: const Icon(Icons.add),
-                )
-              : null,
+            ),
+            TasksList(taskList: tasks)
+          ],
         );
       },
     );
