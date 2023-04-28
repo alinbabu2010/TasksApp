0f76088ce53cfeb6f0711becdd373f10565d20af
Alin Babu <alinbabu2010@gmail.com>
2023-04-28 16:31:15 +0530

    Add date and isFavorite fields in task model

--- lib/models/tasks.dart
+++ lib/models/tasks.dart
@@ -4,33 +4,42 @@ class Task extends Equatable {
   final String title;
   final String description;
   final String id;
+  final String date;
   bool? isDone;
   bool? isDeleted;
+  bool? isFavorite;
 
   Task({
     required this.title,
     required this.description,
     required this.id,
+    required this.date,
     this.isDone,
     this.isDeleted,
+    this.isFavorite,
   }) {
     isDone ??= false;
     isDeleted ??= false;
+    isFavorite ??= false;
   }
 
   Task copyWith({
     String? title,
     String? description,
     String? id,
+    String? date,
     bool? isDone,
     bool? isDeleted,
+    bool? isFavorite,
   }) {
     return Task(
       title: title ?? this.title,
       description: description ?? this.description,
       id: id ?? this.id,
+      date: date ?? this.date,
       isDone: isDone ?? this.isDone,
       isDeleted: isDeleted ?? this.isDeleted,
+      isFavorite: isFavorite ?? this.isFavorite,
     );
   }
 
@@ -39,8 +48,10 @@ class Task extends Equatable {
       'title': title,
       'description': description,
       'id': id,
+      'date': date,
       'isDone': isDone,
       'isDeleted': isDeleted,
+      'isFavorite': isFavorite,
     };
   }
 
@@ -49,11 +60,14 @@ class Task extends Equatable {
       title: map['title'] as String,
       description: map['description'] as String,
       id: map['id'] as String,
+      date: map['date'] as String,
       isDone: map['isDone'] as bool,
       isDeleted: map['isDeleted'] as bool,
+      isFavorite: map['isFavorite'] as bool,
     );
   }
 
   @override
-  List<Object?> get props => [title, id, isDone, isDeleted];
+  List<Object?> get props =>
+      [title, description, id, date, isDone, isDeleted, isFavorite];
 }
