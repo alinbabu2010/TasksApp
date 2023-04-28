9ba4b9e1af3648061232d76d9d1e1db4a86a9cee
Alin Babu <alinbabu2010@gmail.com>
2023-04-28 21:54:59 +0530

    Add extension function for add and edit task bottom sheet

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/material.dart';
+
 import '../models/tasks.dart';
+import '../screens/add_tasks_screen.dart';
 
 extension Filter on List<Task> {
   List<Task> getFavoriteTask() {
@@ -11,5 +14,21 @@ extension Filter on List<Task> {
 
   List<Task> getPendingTask() {
     return where((task) => task.isDone == false).toList();
+  }
+}
+
+extension AddEditTask on BuildContext {
+  void showAddOrEditTaskBottomSheet({Task? task}) {
+    showModalBottomSheet(
+      context: this,
+      isScrollControlled: true,
+      builder: (context) => SingleChildScrollView(
+        child: Container(
+          padding:
+              EdgeInsets.only(bottom: MediaQuery.of(context).viewInsets.bottom),
+          child: AddTasksScreen(task: task),
+        ),
+      ),
+    );
   }
 }
