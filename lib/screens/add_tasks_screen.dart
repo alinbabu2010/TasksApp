99c755c2b89ee90f4a6db8017caea1179db62741
Alin Babu <alinbabu2010@gmail.com>
2023-04-28 22:01:07 +0530

    Refactor add task screen to reuse for task edit
    Add function to handle input submission and field task.

--- lib/screens/add_tasks_screen.dart
+++ lib/screens/add_tasks_screen.dart
@@ -6,21 +6,46 @@ import 'package:tasks_app/utils/guid_gen.dart';
 import '../models/tasks.dart';
 
 class AddTasksScreen extends StatelessWidget {
+  final Task? task;
+
   const AddTasksScreen({
     super.key,
+    this.task,
   });
+
+  void _onSubmit(String title, String description, BuildContext context) {
+    if (task != null) {
+      context.read<TasksBloc>().add(UpdateTask(
+            task: (task?.copyWith(title: title, description: description))!,
+            isEdit: true,
+          ));
+    } else {
+      var task = Task(
+        title: title,
+        description: description,
+        date: DateTime.now().toString(),
+        id: GUIDGen.generate(),
+      );
+      context.read<TasksBloc>().add(AddTask(task: task));
+    }
+    Navigator.pop(context);
+  }
 
   @override
   Widget build(BuildContext context) {
-    TextEditingController titleController = TextEditingController();
-    TextEditingController descriptionController = TextEditingController();
+    TextEditingController titleController =
+        TextEditingController(text: task?.title);
+    titleController.selection =
+        TextSelection.collapsed(offset: titleController.text.length);
+    TextEditingController descriptionController =
+        TextEditingController(text: task?.description);
     return Padding(
       padding: const EdgeInsets.all(Dimens.addTaskPadding),
       child: Column(
         children: [
-          const Text(
-            "Add Task",
-            style: TextStyle(fontSize: Dimens.addTaskFontSize),
+          Text(
+            task != null ? "Edit Task" : "Add Task",
+            style: const TextStyle(fontSize: Dimens.addTaskFontSize),
           ),
           const SizedBox(height: Dimens.addTaskSizedBoxHeight),
           TextField(
@@ -48,17 +73,9 @@ class AddTasksScreen extends StatelessWidget {
                 child: const Text("Cancel"),
               ),
               ElevatedButton(
-                  onPressed: () {
-                    var task = Task(
-                      title: titleController.text,
-                      description: descriptionController.text,
-                      date: DateTime.now().toString(),
-                      id: GUIDGen.generate(),
-                    );
-                    context.read<TasksBloc>().add(AddTask(task: task));
-                    Navigator.pop(context);
-                  },
-                  child: const Text('Add')),
+                  onPressed: () => _onSubmit(titleController.text,
+                      descriptionController.text, context),
+                  child: Text(task == null ? 'Add' : 'Save')),
             ],
           )
         ],
