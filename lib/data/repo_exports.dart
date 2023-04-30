44542476239f39dd9a8ceed0a9db11928acc5c97
Alin Babu <alinbabu2010@gmail.com>
2023-04-30 13:47:29 +0530

    Add task repository exports

--- lib/data/repo_exports.dart
+++ lib/data/repo_exports.dart
@@ -1,2 +1,4 @@
 export 'repository/auth_repository.dart';
 export 'repository/firebase_auth_repository.dart';
+export 'repository/firestore_repository.dart';
+export 'repository/tasks_repository.dart';
