7e8c4777656d9534c9ee6c07f616a7a7ddb25b17
Alin Babu <alinbabu2010@gmail.com>
2023-04-30 10:58:41 +0530

    Add stream builder to listen firebase user changes for setting homeRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:tasks_app/screens/login_screen.dart';
 import 'package:tasks_app/utils/app_route.dart';
 import 'package:tasks_app/utils/app_theme.dart';
 
@@ -43,15 +43,19 @@ class TasksApp extends StatelessWidget {
       child: BlocBuilder<ThemeBloc, ThemeState>(
         builder: (context, state) {
           final customThemeMode =
-              state.isDarkTheme ? ThemeMode.dark : ThemeMode.light;
+          state.isDarkTheme ? ThemeMode.dark : ThemeMode.light;
           final themeMode =
-              state.isSystemTheme ? ThemeMode.system : customThemeMode;
+          state.isSystemTheme ? ThemeMode.system : customThemeMode;
           return MaterialApp(
             onGenerateTitle: (context) => S.of(context).appTitle,
             theme: AppThemes.appThemeData[AppTheme.lightTheme],
             debugShowCheckedModeBanner: false,
             darkTheme: AppThemes.appThemeData[AppTheme.darkTheme],
-            home: const LoginScreen(),
+            home: StreamBuilder<User?>(
+              stream: FirebaseAuth.instance.authStateChanges(),
+              builder: (context, snapshot) =>
+                  appRouter.getHomeRoute(snapshot.data),
+            ),
             themeMode: themeMode,
             onGenerateRoute: appRouter.onGenerateRoute,
             localizationsDelegates: const [
