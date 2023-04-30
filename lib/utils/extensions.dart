a83816b90a3de8c1ce93ab0bdf48f920e47677f1
Alin Babu <alinbabu2010@gmail.com>
2023-04-30 14:35:40 +0530

    Add extension for getting removed tasks and modify other list extensions

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -4,17 +4,15 @@ import '../models/tasks.dart';
 import '../screens/add_edit_ task_screen.dart';
 
 extension Filter on List<Task> {
-  List<Task> getFavoriteTask() {
-    return where((task) => task.isFavorite == true).toList();
-  }
+  List<Task> get favoriteTask =>
+      where((task) => task.isFavorite == true).toList();
 
-  List<Task> getCompletedTask() {
-    return where((task) => task.isDone == true).toList();
-  }
+  List<Task> get completedTask => where((task) => task.isDone == true).toList();
 
-  List<Task> getPendingTask() {
-    return where((task) => task.isDone == false).toList();
-  }
+  List<Task> get pendingTask => where((task) => task.isDone == false).toList();
+
+  List<Task> get removedTask =>
+      where((task) => task.isDeleted == true).toList();
 }
 
 extension AddEditTask on BuildContext {
